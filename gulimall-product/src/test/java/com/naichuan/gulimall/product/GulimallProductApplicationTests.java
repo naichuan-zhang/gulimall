6d3527611d693f41c6150639a42146751c39487c
znc <naichuanzhang@126.com>
2021-11-19 15:28:59 +0800

    Initial commit

---
+++ gulimall-product/src/test/java/com/naichuan/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naichuan.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
