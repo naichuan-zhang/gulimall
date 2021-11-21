25590744a1a5292e2a76648c1720ed1f4fa56e99
Naichuan Zhang <zhangnaichuan168@gmail.com>
2021-11-21 18:50:25 +0800

    sys模块添加配置类

---
+++ renren-fast/src/main/java/io/renren/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package io.renren.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Naichuan Zhang
+ * @since 11/21/2021
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(security());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("人人开源")
+                .description("renren-fast文档")
+                .termsOfServiceUrl("https://www.renren.io")
+                .version("3.0.0")
+                .build();
+    }
+
+    private List<ApiKey> security() {
+        return Collections.singletonList(
+                new ApiKey("token", "token", "header"));
+    }
+}
